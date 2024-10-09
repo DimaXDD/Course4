76bbd00f9b62be7d96c537b17953473b3f20dd3a
DimaDD <dimatruba2004@yandex.ru>
2024-10-09 20:29:49 +0300

    delete

--- 7sem/RIS (Тимонович)/Lab4_6/mainServer/routes/router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controller/controller');
-
-router.get('/getCurrTime', controller.getCurrTime);
-
-module.exports = router;
--- 7sem/RIS (Тимонович)/Lab4_6/server1/routes/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/controller');
-
-// Роутеры для обработки процесса репликации
-router.get('/replicate', controller.pullReplicationController);
-router.post('/replicate', controller.pushReplicationController);
-
-module.exports = router;
